7bec9b18b7ffa68b0d06ffb9388ce0f9adf86bad
RahulGhosal <83332647+RahulGhosal@users.noreply.github.com>
2024-12-13 18:05:05 +0530

    Add files via upload

--- File.spec.js
+++ File.spec.js
@@ -2,18 +2,24 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://d3pv22lioo8876.cloudfront.net/tiptop/');
-  await page.getByLabel('Name *').click();
-  await page.getByLabel('Name *').fill('Test name');
-  await page.getByLabel('Name *').press('Tab');
-  await page.getByLabel('Password *').fill('Test');
-  await page.getByLabel('Readonly input').click();
+  await page.getByText('Verify that the text input element with xpath .//input[@name=\'my-disabled\'] is').click();
+  
+  
   await page.getByLabel('Select Color White Violet').selectOption('violet');
+  await page.getByText('Select Color White Violet').click();
   await page.getByLabel('Select Color White Violet').selectOption('indigo');
+  await page.getByText('Select Color White Violet').click();
   await page.getByLabel('Select Color White Violet').selectOption('blue');
-  await page.getByLabel('Select Color White Violet').selectOption('green');
   await page.getByLabel('Select Color White Violet').selectOption('yellow');
+  await page.getByText('Select Color White Violet').click();
   await page.getByLabel('Select Color White Violet').selectOption('orange');
   await page.getByLabel('Select Color White Violet').selectOption('red');
+  await page.getByLabel('Name *').click();
+  await page.getByLabel('Name *').fill('Test');
+  await page.locator('div').nth(3).click();
+  await page.getByLabel('Readonly input').click();
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('Test');
   await page.getByRole('button', { name: 'Submit' }).click();
   await page.getByText('Received!').click();
 });
